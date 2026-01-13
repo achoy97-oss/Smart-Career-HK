22f7176ba2f75ae4c518471a121cfe7a58b7e7fd
achoy97-oss <achoy97@gmail.com>
2026-01-13 07:09:13 +0000

    hide secret

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ class Config:
     """Configuration settings"""
     
     # Pinecone
-    PINECONE_API_KEY = "pcsk_5PQhRu_AaCxW5SSdDkG1Ue63VJBYZffwpxW8tKnwMsvM3T3JJ4fuVrp3Wv7tq9fXVUfQKB"
+    PINECONE_API_KEY = "xxx"
     PINECONE_ENVIRONMENT = "us-east-1"
     INDEX_NAME = "job-resume-matcher"
     EMBEDDING_DIMENSION = 384
     MODEL_NAME = "all-MiniLM-L6-v2"
     
     # Azure OpenAI
-    AZURE_ENDPOINT = "https://hkust.azure-api.net/"
-    AZURE_API_KEY = "eacb49eb7c904d738c7644fc104aa7bb"
+    AZURE_ENDPOINT = "XXX"
+    AZURE_API_KEY = "XXX"
     AZURE_API_VERSION = "2024-10-21"
     AZURE_MODEL = "gpt-4o-mini"
     
     # RapidAPI
-    #RAPIDAPI_KEY = "2cae75c0f7msh1cb2821ab20dbabp11a63fjsn71bad75cf885"
-    RAPIDAPI_KEY = "9331fddba1mshcf8b918c98307c3p1090c5jsndc50de14b9b4"
+    #RAPIDAPI_KEY = "XXX"
+    RAPIDAPI_KEY = "XXX"
     
     # Settings
     MAX_JOBS_TO_FETCH = 50
